7ffb42216b6c545aecf9c8db035e2a96c0ef5de2
Manuel Ernst <manuel.ernst@methodpark.de>
2018-11-30 09:43:19 +0100

    initial

---
+++ src/lib/state/__tests__/stopwatch.js
@@ -0,0 +1,130 @@
+import sinon from 'sinon';
+import ms from 'ms';
+
+import stopwatchReducer, {
+  createStopwatchAddAction, createStopwatchStartAction, createStopwatchUpdateAction,
+  SW_STATE_ACTIVE, SW_STATE_DONE, SW_STATE_PENDING
+} from '../stopwatch';
+
+const regexpUUID = /[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+/;
+
+describe('stopwatch', () => {
+  describe('no action', () => {
+    it('should', () => {
+      const action = {};
+      const state = undefined;
+  
+      const newState = stopwatchReducer(state, action);
+  
+      expect(newState).toEqual([]);
+    });
+  });
+
+  describe('add stopwatch', () => {
+    it('should create a stopwatch', () => {
+      const action = createStopwatchAddAction(ms('3s'), {my: 'payload'});
+      const state = undefined;
+  
+      const newState = stopwatchReducer(state, action);
+  
+      expect(newState).toEqual([{
+        id: expect.stringMatching(regexpUUID),
+        state: 'SW:PENDING',
+        startTime: null,
+        timeLeft: null,
+        payload: {my: 'payload'},
+        duration: 3000
+      }]);
+    });
+  });
+
+  describe('start stopwatch', () => {
+    let clock = null;
+    beforeAll(() => {
+      clock = sinon.useFakeTimers();
+    });
+
+    afterAll(() => clock.restore());
+
+    it('should start a stopwatch', () => {
+      const id = 'myId';
+      const action = createStopwatchStartAction(id);
+      const state = [{
+        id,
+        state: SW_STATE_PENDING,
+        startTime: null,
+        timeLeft: null,
+        payload: {my: 'payload'},
+        duration: ms('3s')
+      }];
+  
+      const newState = stopwatchReducer(state, action);
+
+      expect(newState).toEqual([{
+        id,
+        state: SW_STATE_ACTIVE,
+        duration: ms('3s'),
+        startTime: '1970-01-01T00:00:00.000Z',
+        payload: {my: 'payload'},
+        timeLeft: null
+      }]);
+    });
+  });
+
+  describe('update stopwatch', () => {
+    let clock = null;
+    beforeEach(() => {
+      clock = sinon.useFakeTimers(ms('10s'));
+    });
+
+    afterEach(() => clock.restore());
+
+    it('should update a stopwatch in between', () => {
+      const id = 'myId';
+      const action = createStopwatchUpdateAction(id);
+      const state = [{
+        id,
+        state: SW_STATE_ACTIVE,
+        startTime: '1970-01-01T00:00:00.000Z',
+        timeLeft: null,
+        payload: {my: 'payload'},
+        duration: ms('10m')
+      }];
+  
+      const newState = stopwatchReducer(state, action);
+
+      expect(newState).toEqual([{
+        id,
+        state: SW_STATE_ACTIVE,
+        duration: ms('10m'),
+        startTime: '1970-01-01T00:00:00.000Z',
+        payload: {my: 'payload'},
+        timeLeft: '9m 50s'
+      }]);
+    });
+
+    it('should update a stopwatch thats done', () => {
+      const id = 'myId';
+      const action = createStopwatchUpdateAction(id);
+      const state = [{
+        id,
+        state: SW_STATE_ACTIVE,
+        startTime: '1970-01-01T00:00:00.000Z',
+        timeLeft: null,
+        payload: {my: 'payload'},
+        duration: ms('10s')
+      }];
+  
+      const newState = stopwatchReducer(state, action);
+
+      expect(newState).toEqual([{
+        id,
+        state: SW_STATE_DONE,
+        duration: ms('10s'),
+        startTime: '1970-01-01T00:00:00.000Z',
+        payload: {my: 'payload'},
+        timeLeft: '0ms'
+      }]);
+    });
+  });
+});
---
+++ src/lib/state/stopwatch.js
@@ -0,0 +1,69 @@
+import uuid from 'uuid/v4';
+import moment from 'moment';
+import prettyMs from 'pretty-ms';
+
+export const SW_STATE_PENDING = 'SW:PENDING';
+export const SW_STATE_ACTIVE = 'SW:ACTIVE';
+export const SW_STATE_DONE = 'SW:DONE';
+
+const STOPWATCH_ADD = 'SW:ADD';
+const STOPWATCH_START = 'SW:START';
+const STOPWATCH_UPDATE = 'SW:UPDATE';
+
+export function createStopwatchAddAction(duration, payload) {
+  return {type: STOPWATCH_ADD, duration, payload};
+}
+
+export function createStopwatchStartAction(id) {
+  return {type: STOPWATCH_START, id};
+}
+
+export function createStopwatchUpdateAction(id) {
+  return {type: STOPWATCH_UPDATE, id};
+}
+
+export default function reducer(state=[], action) {
+  if (action.type === STOPWATCH_ADD) {
+    return state.concat([createStopwatch(action.duration, action.payload)]);
+  }
+
+  if (action.type === STOPWATCH_START) {
+    return state.map(sw => {
+      if (sw.id !== action.id) return sw;
+
+      return {
+        ...sw,
+        state: SW_STATE_ACTIVE,
+        startTime: (new Date()).toISOString()
+      }
+    });
+  }
+
+  if (action.type === STOPWATCH_UPDATE) {
+    return state.map(sw => {
+      if (sw.id !== action.id) return sw;
+
+      const elapsedSinceStart = moment().diff(sw.startTime);
+      const timeLeft = sw.duration - elapsedSinceStart;
+
+      return {
+        ...sw,
+        state: timeLeft <= 0 ? SW_STATE_DONE : SW_STATE_ACTIVE,
+        timeLeft: prettyMs(timeLeft)
+      };
+    });
+  }
+
+  return state;
+}
+
+function createStopwatch(duration, payload) {
+  return {
+    id: uuid(),
+    state: SW_STATE_PENDING,
+    startTime: null,
+    timeLeft: null,
+    payload,
+    duration
+  };
+}
