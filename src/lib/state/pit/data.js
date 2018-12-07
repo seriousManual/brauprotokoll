ba67dd4f00cd812c19937ea2a2034498ce5c0bf0
Manuel Ernst <manuel.ernst@methodpark.de>
2018-12-07 08:13:20 +0100

    rewrite pits

--- src/lib/state/pit/data.js
+++ src/lib/state/pit/data.js
@@ -1,36 +1,60 @@
-import uuid from 'uuid/v4';
-
 const PIT_ADD = 'PIT:ADD';
 const PIT_SET = 'PIT:SET';
+const PIT_ACTIVATION = 'PIT:ACTIVATION';
+const PIT_CLEARANCE = 'PIT:CLEARANCE';
 
-export function createPitAddAction(payload) {
-  return {type: PIT_ADD, payload};
+export function createPitAddAction(ident, payload) {
+  return {type: PIT_ADD, ident, payload};
 }
 
-export function createPitSetAction(id) {
-  return {type: PIT_SET, id};
+export function createPitSetAction(ident) {
+  return {type: PIT_SET, ident};
+}
+
+export function createPitActivationAction(ident, flag) {
+  return {type: PIT_ACTIVATION, ident, flag};
+}
+
+export function createPitClearanceAction(ident, flag) {
+  return {type: PIT_CLEARANCE, ident, flag};
 }
 
 export default function reducer(state=[], action) {
   if (action.type === PIT_ADD) {
-    return state.concat([createPit(action.payload)]);
+    return state.concat([createPit(action.ident, action.payload)]);
   }
 
   if (action.type === PIT_SET) {
-    return state.map(pit => {
-      if (pit.id !== action.id) return pit;
+    return _handlePitForIdent(state, action.ident, pit => {
+      return {...pit, pointInTime: (new Date()).toISOString()}
+    });
+  }
 
-      return {...pit, pointInTime: (new Date()).toISOString()};
+  if (action.type === PIT_ACTIVATION) {
+    return _handlePitForIdent(state, action.ident, pit => {
+      return {...pit, activated: action.flag}
+    });
+  }
+
+  if (action.type === PIT_CLEARANCE) {
+    return _handlePitForIdent(state, action.ident, pit => {
+      return {...pit, clearance: action.flag}
     });
   }
 
   return state;
 }
 
-function createPit(payload) {
+function createPit(ident, payload) {
   return {
-    id: uuid(),
-    pointInTime: null,
-    payload
+    ident,
+    payload,
+    activated: false,
+    clearance: false,
+    pointInTime: null
   };
+}
+
+function _handlePitForIdent(state, ident, handler) {
+  return state.map(pit => pit.ident !== ident ? pit : handler(pit));
 }
--- src/lib/state/pit/pit.test.js
+++ src/lib/state/pit/pit.test.js
@@ -1,8 +1,9 @@
 import sinon from 'sinon';
 
-import pitReducer, {createPitAddAction, createPitSetAction} from './data';
-
-const regexpUUID = /[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+\-[a-f0-9]+/;
+import pitReducer, {
+  createPitAddAction, createPitSetAction, 
+  createPitActivationAction, createPitClearanceAction
+} from './data';
 
 describe('pit', () => {
   describe('data', () => {
@@ -19,15 +20,18 @@ describe('pit', () => {
 
     describe('add pit', () => {
       it('should create a pit', () => {
-        const action = createPitAddAction({ my: 'payload' });
+        const ident = 'myIdent';
+        const action = createPitAddAction(ident, { my: 'payload' });
         const state = undefined;
 
         const newState = pitReducer(state, action);
 
         expect(newState).toEqual([{
-          id: expect.stringMatching(regexpUUID),
-          pointInTime: null,
-          payload: { my: 'payload' }
+          ident,
+          payload: { my: 'payload' },
+          activated: false,
+          clearance: false,
+          pointInTime: null
         }]);
       });
     });
@@ -41,22 +45,102 @@ describe('pit', () => {
       afterAll(() => clock.restore());
 
       it('should set a pit', () => {
-        const id = 'myId';
-        const action = createPitSetAction(id);
-        const state = [{
-          id,
-          pointInTime: null,
-          payload: { my: 'payload' }
-        }];
+        const ident = 'myIdent';
+        const entry1 = {
+          ident,
+          payload: {my: 'payload'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const entry2 = {
+          ident: 'myIdent2',
+          payload: {my: 'payload2'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const action = createPitSetAction(ident);
+        const state = [entry1, entry2];
 
         const newState = pitReducer(state, action);
 
-        expect(newState).toEqual([{
-          id,
-          pointInTime: null,
-          payload: { my: 'payload' },
-          pointInTime: '1970-01-01T00:00:00.000Z'
-        }]);
+        expect(newState).toEqual([
+          {...entry1, pointInTime: '1970-01-01T00:00:00.000Z'}, 
+          entry2
+        ]);
+      });
+    });
+
+    describe('activate pit', () => {
+      let clock = null;
+      beforeAll(() => {
+        clock = sinon.useFakeTimers();
+      });
+
+      afterAll(() => clock.restore());
+
+      it('should set a pit', () => {
+        const ident = 'myIdent';
+        const entry1 = {
+          ident,
+          payload: {my: 'payload'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const entry2 = {
+          ident: 'myIdent2',
+          payload: {my: 'payload2'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const action = createPitActivationAction(ident, true);
+        const state = [entry1, entry2];
+
+        const newState = pitReducer(state, action);
+
+        expect(newState).toEqual([{...entry1, activated: true}, entry2]);
+      });
+    });
+
+    describe('pit clearance', () => {
+      let clock = null;
+      beforeAll(() => {
+        clock = sinon.useFakeTimers();
+      });
+
+      afterAll(() => clock.restore());
+
+      it('should set a pit', () => {
+        const ident = 'myIdent';
+        const entry1 = {
+          ident,
+          payload: {my: 'payload'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const entry2 = {
+          ident: 'myIdent2',
+          payload: {my: 'payload2'},
+          activated: false,
+          clearance: false,
+          pointInTime: null
+        };
+
+        const action = createPitClearanceAction(ident, true);
+        const state = [entry1, entry2];
+
+        const newState = pitReducer(state, action);
+
+        expect(newState).toEqual([{...entry1, clearance: true}, entry2]);
       });
     });
   });
